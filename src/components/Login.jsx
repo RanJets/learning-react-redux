8c12532254ad1e43536fcdd3311e7fa4015e14dc
RanJets <rmdejesus27@gmail.com>
2022-12-07 21:46:23 +0800

    Redux tutorial basic finished

---
+++ src/components/Login.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../features/user";
+
+const Login = () => {
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          user.status
+            ? dispatch(logout())
+            : dispatch(
+                login({
+                  name: "Rhandell",
+                  age: 20,
+                  email: "dell@gmail.com",
+                  status: true,
+                })
+              );
+        }}
+      >
+        {user.status ? "Logout" : "Login"}
+      </button>
+    </div>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userReducer from "./features/user";
+import themeReducer from "./features/theme";
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    theme: themeReducer,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
