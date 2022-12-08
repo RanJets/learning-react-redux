946e1f66c20f26319e5dab703b8e2c2358a32935
RanJets <rmdejesus27@gmail.com>
2022-12-08 20:07:19 +0800

    Redux Crud finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import AddUser from "./components/AddUser";
+import DisplayUsers from "./components/DisplayUsers";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AddUser />
+      <DisplayUsers />
+    </div>
+  );
 }
 
 export default App;
