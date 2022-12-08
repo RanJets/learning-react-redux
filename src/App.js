ee884a160b7aea7e574f8eba8c91aca5461e67f5
RanJets <rmdejesus27@gmail.com>
2022-12-08 17:32:27 +0800

    initializing crud in redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
-import ChangeColor from "./components/ChangeColor";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <Profile />
-      <Login />
-      <ChangeColor />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
